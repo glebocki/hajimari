9f20e701a30a91a72651110239061e33892cad50
Przemyslaw Glebocki <glebocki.przemyslaw@gmail.com>
2021-07-24 19:09:02 +0200

    Loading H5 minst model and predicting against an img

--- ml/load-ml.py
+++ ml/load-ml.py
@@ -1,11 +1,16 @@
+# Loads a mnist H5 model
+# Pick a few of the numbers from data set and try to predict what number the represent.
+
 import os
 
 import tensorflow as tf
 from tensorflow import keras
 
+import numpy as np
+
 # Lodaing model H5
 
-model = keras.models.load_model("my_model.h5")
+model: tf.keras.Model = keras.models.load_model("my_model.h5")
 
 # Re-evaluate the model
 
@@ -17,5 +22,23 @@ test_labels = test_labels[:1000]
 train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
 test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
 
+# Evaluate the restored model
 loss, acc = model.evaluate(test_images, test_labels, verbose=2)
 print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
+
+print(model.predict(test_images).shape)
+# print(model.predict(test_images))
+
+class_names = ["0", "1", "2", "3", "4",
+               "5", "6", "7", "8", "9"]
+
+X_test = test_images
+X_imgsToBePredicted = X_test[:3]
+y_pred = model.predict(X_imgsToBePredicted)
+# Printing the predicted labels
+print(y_pred)
+print("###")
+print(y_pred[0])
+for i in range(3):
+    print(class_names[np.argmax(y_pred[i])])
+
