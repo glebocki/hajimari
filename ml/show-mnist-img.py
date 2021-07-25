efbc4d74a32b3b42779d1d9d3c90d2686034f8f0
Przemyslaw Glebocki <glebocki.przemyslaw@gmail.com>
2021-07-25 22:56:12 +0200

    Clean up example of showing mnist img

--- ml/show-mnist-img.py
+++ ml/show-mnist-img.py
@@ -1,32 +1,38 @@
 # Show a mnist image from dataset
 
+import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
+
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-import matplotlib.pyplot as plt
+
 # %matplotlib inline
 
-# pick a sample to plot
-sample = 3
-image = X_test[sample]
-# plot the sample
-fig = plt.figure
-plt.imshow(image, cmap='gray')
-plt.show()
+def plot_single_sample():
+    # pick a sample to plot
+    sample = 111
+    image = X_test[sample]
+    # plot the sample
+    fig = plt.figure
+    plt.imshow(image, cmap='gray')
+    plt.show()
 
-# num_row = 10
-# num_col = 10
-#
-# num = num_row * num_col
-# images = X_train[:num]
-# labels = Y_train[:num]
-#
-# # plot images
-# fig, axes = plt.subplots(num_row, num_col, figsize=(1.5*num_col,2*num_row))
-# for i in range(num):
-#     ax = axes[i//num_col, i%num_col]
-#     ax.imshow(images[i], cmap='gray')
-#     ax.set_title('Label: {}'.format(labels[i]))
-# plt.tight_layout()
-# plt.show()
 
+def plot_multiple_samples(rows: int, columns: int):
+    num = rows * columns
+    images = X_train[:num]
+    labels = Y_train[:num]
+
+    # plot images
+    fig, axes = plt.subplots(rows, columns, figsize=(1.5 * columns, 2 * rows))
+    for i in range(num):
+        ax = axes[i // columns, i % columns]
+        ax.imshow(images[i], cmap='gray')
+        ax.set_title('Number: {}'.format(labels[i]))
+
+    plt.tight_layout()
+    plt.show()
+
+
+plot_single_sample()
+plot_multiple_samples(10, 10)
