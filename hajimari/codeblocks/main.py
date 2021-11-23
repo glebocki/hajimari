f62d16c89f2ba9b1af2715ab332fd5d6766f9e6c
Przemyslaw Glebocki <glebocki.przemyslaw@gmail.com>
2021-11-23 10:55:10 +0100

    Generator: Working microservice example

--- hajimari/codeblocks/main.py
+++ hajimari/codeblocks/main.py
@@ -2,6 +2,7 @@ from typing import List
 
 import numpy as np
 import tensorflow as tf
+import uvicorn
 from fastapi import FastAPI
 from numpy import ndarray
 from tensorflow import keras
@@ -11,17 +12,21 @@ app = FastAPI()
 class_names = ["0", "1", "2", "3", "4",
                "5", "6", "7", "8", "9"]
 
+# TODO: Load once - Singleton, research Dependency Injection in FastApi
+# Loading model H5
+try:
+    model: tf.keras.Model = keras.models.load_model("mnist_model.h5")
+except OSError:
+    print("Oops! Could not load Machine Learning model. Make sure it is in the same directory as the service.")
+    raise
+
 
 @app.post("/api/ml/predict")
 def ml_upload_img(payload: List[List[float]]):
-    input = np.array(payload)
-
-    # TODO: Load once - Singleton, research Dependency Injection in FastApi
-    # Loading model H5
-    model: tf.keras.Model = keras.models.load_model("mnist_model.h5")
+    predict_input = np.array(payload)
 
     # PREDICT
-    predictions: ndarray = model.predict(input)
+    predictions: ndarray = model.predict(predict_input)
 
     predictions_list: list = predictions.tolist()
     return {
@@ -40,6 +45,9 @@ def human_readable_answers(predictions):
     for i in range(len(predictions)):
         argmax = np.argmax(predictions[i])
         answer = class_names[argmax]
-        print(answer)
         answers.append(answer)
     return answers
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
